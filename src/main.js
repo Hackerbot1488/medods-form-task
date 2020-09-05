269e10a0397ab01e3ad9c632d9adbce680c30f13
Hackerbot1488 <gromovartem04@mail.ru>
2020-09-05 15:33:04 +0300

    added devtools to onfig

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import Vuelidate from 'vuelidate'
 Vue.use(Vuelidate)
+Vue.config.devtools = true
 Vue.config.productionTip = false;
 
 new Vue({
